4657ffaefbdca8b6b7fd2a85ca4ed0228f2a0c2a
botzko <b.aleksiew@gmail.com>
2023-07-05 00:07:38 +0300

    Config added

--- vue/firebase.js
+++ vue/firebase.js
@@ -1,5 +1,10 @@
 var firebaseConfig = {
-    // Add your Firebase configuration here
+    apiKey: "AIzaSyCvu5huN46Jst9rGVzJT_vnlkt_D27PZ-U",
+    authDomain: "vue-app-84.firebaseapp.com",
+    projectId: "vue-app-84",
+    storageBucket: "vue-app-84.appspot.com",
+    messagingSenderId: "643284874972",
+    appId: "1:643284874972:web:9430f0ca598188d9aea619"
   };
   
   firebase.initializeApp(firebaseConfig);

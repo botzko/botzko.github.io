8edb8373dfc025eaba7a387b985bfd06145a6d1c
botzko <b.aleksiew@gmail.com>
2023-07-08 07:55:19 +0300

    Data

---
+++ vue/data.js
@@ -0,0 +1,15 @@
+let parcalki = {
+    id : 1,
+    title : "Някакво лятно парцалче!",
+    description : "Рокля на авокадота.",
+    price : 254,
+    images : ['image1.jpg','image2.jpg','image3.jpg'],
+    categories : ['Summer','Winter','Autumn'],
+    tags : ['Skirts','Scarfs','Snikers'],
+    sheree : ["Kami", "Magi", "Maria"],
+    periods : {
+        1 : ["28/07/23", "07/08/23", "Kami"],
+        2 : ["08/08/23", "14/08/23", "Magi"],
+        3 : ["28/10/23", "07/11/23", "Kami"],
+    }
+};

b23f9d826b4e39734431a4c75e2bc30577866564
botzko <b.aleksiew@gmail.com>
2023-08-04 10:39:20 +0300

    n

---
+++ nvue/components/About.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+      <h2>About Page</h2>
+      <p>Welcome to the About page!</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'About',
+  };
+  </script>
+  
